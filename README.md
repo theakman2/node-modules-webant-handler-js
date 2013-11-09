a3e951348a788f3e8eda7e7bc23cb31dd7a6e55b
theakman2 <theakman2@hotmail.com>
2013-11-09 12:45:03 +0000

    API changes to accommodate webant v0.8.0.

--- README.md
+++ README.md
@@ -12,38 +12,24 @@ If for some reason you'd like to use the module outside of webant, install as fo
 
 ## Usage
 
-Ensure the `js` handler is present in your webant configuration file.
-
-An example configuration file which uses this handler may look like this:
+Ensure the `js` handler is present in your webant configuration file. For example:
 
 ````json
 {
-    "jsEntryPath":"%%base%%/src/js/main.js",
-    "jsDestPath":"%%base%%/build/main.js",
+    "entry":"src/js/main.js",
+    "dest":"build/main.js",
     "handlers":{
         "js":{}
     }
 }
 ````
 
-You may now `require` javascript files via the function style:
+You may now `require` javascript files:
 
 ````javascript
 var foo = require("../lib/foo.js");
 foo.sayHello();
 ````
-
-## Settings
-
-The following configuration settings are available:
-
-`mode`
-
-Can be either `normal` (default), `compress` or `debug`. Setting to `compress` will use Uglify-JS to compress each required javascript file. Setting to `debug` will output the filepath and line number of each line for debugging purposes, relative to the `debugBasePath` setting.
-
-`debugBasePath`
-
-A path that filepaths are relative to if `mode` is set to `debug`. 
 
 ## Tests
 
--- test/index.js
+++ test/index.js
@@ -1,128 +1,36 @@
 var Handler = require("../lib/index.js");
-var HandlerBase = require("./lib/Handler.js");
-
-function createHandler(Handler,settings) {
-	var handlerBase = new HandlerBase(settings);
-	
-	Handler.prototype = handlerBase;
-	Handler.prototype.constructor = Handler;
-	
-	return new Handler();
-}
-
-var uglify = require("uglify-js");
 
 var tests = {
-		"test js 1":function(assert,done) {
-			var handler = createHandler(Handler);
-			var data = {
-				filePath:"https://sfi9s.sdf.sd/vk93k.handlebars?bla=3",
-				raw:"https://sfi9s.sdf.sd/vk93k.handlebars?bla=3",
-				requireType:"comment"
-			};
-			handler.willHandle(data,function(err,yes){
-				assert.strictEqual(err,null,"Handler should not report any errors.");
-				assert.strictEqual(yes,false,"Handler should not claim to handle this file.");
-				done();
-			});
-		},
-		"test js 2":function(assert,done) {
-			var handler = createHandler(Handler);
-			var data = {
-				filePath:__dirname+"/path/to/bad/file.json",
-				raw:"file.json",
-				requireType:"function"
-			};
-			handler.willHandle(data,function(err,yes){
-				assert.strictEqual(err,null,"Handler should not report any errors.");
-				assert.strictEqual(yes,false,"Handler should not claim to handle this file.");
-				done();
-			});
-		},
-		"test js correct file type normal":function(assert,done) {
-			var handler = createHandler(Handler);
+	"test filetypes":function(assert) {
+		var handler = new Handler();
+		var data = [
+		            "https://mysite.co.uk/bla.js",
+		            "//cdn.google.com/path/to/assets.css",
+		            "path/to/assets.js",
+		            "/abs/path/to/assets.js",
+		            "@@hbs/runtime",
+		            "@@css/addStylesheet"
+		            ];
+		assert.deepEqual(
+			data.map(handler.willHandle),
+			[false,false,true,true,false,false],
+			"Should handle the correct files."
+		);
+	},
+	"test content":function(assert,done) {
+		var handler = new Handler();
+		
+		handler.handle(__dirname+"/script.js",function(err,content){
+			assert.ok(!err,"There should be no errors handling this file.");
+			assert.equal(
+				content,
+				"var pi = 3.14; function foo() {}; function bar() { alert('b'); }; bar();",
+				"Handler should return the correct content."
+			);
 			
-			var payload = {
-				requireType:"function",
-				filePath:__dirname+"/script.js",
-				raw:"./script.js"
-			};
-			
-			handler.handle(payload,function(resp){
-				assert.deepEqual(
-					resp,
-					{
-						content:"var pi = 3.14; function foo() {}; function bar() { alert('b'); }; bar();",
-						type:"internalJs"
-					},
-					"Handler should call 'update' correctly."
-				);
-				
-				done();
-			});
-		},
-		"test js correct file type compress":function(assert,done) {
-			var handler = createHandler(Handler,{mode:'compress'});
-			
-			var payload = {
-				requireType:"function",
-				filePath:__dirname+"/script.js",
-				raw:"./script.js"
-			};
-			
-			handler.handle(payload,function(resp){
-				assert.strictEqual(
-					resp.hasOwnProperty("content"),
-					true,
-					"Handler should call 'update' correctly (1)."
-				);
-				
-				assert.strictEqual(
-					resp.type,
-					"internalJs",
-					"Handler should call 'update' correctly (2)."
-				);
-				
-				assert.strictEqual(
-					resp.content.length < 72,
-					true,
-					"Handler should call 'update' correctly (3)."
-				);
-				
-				done();
-			});
-		},
-		"test js correct file type debug":function(assert,done) {
-			var handler = createHandler(Handler,{mode:'debug',debugBasePath:__dirname});
-			
-			var payload = {
-				requireType:"function",
-				filePath:__dirname+"/script.js",
-				raw:"./script.js"
-			};
-			
-			handler.handle(payload,function(resp){
-				assert.strictEqual(
-					resp.hasOwnProperty("content"),
-					true,
-					"Handler should call 'update' correctly (1)."
-				);
-				
-				assert.strictEqual(
-					resp.type,
-					"internalJs",
-					"Handler should call 'update' correctly (2)."
-				);
-				
-				assert.strictEqual(
-					resp.content.indexOf("/*script.js: 1*/") === 0,
-					true,
-					"Handler should call 'update' correctly (3)."
-				);
-				
-				done();
-			});
-		}
+			done();
+		});
+	}
 };
 
 require("test").run(tests);
